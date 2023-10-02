b51c7a48260b6080f7fd43e9bfa5c9a3146389d9
asands1769 <asands1769@outlook.com>
2023-10-02 19:19:49 -0400

    finishing initial studio

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -64,6 +64,7 @@ public class SpaDayController {
         }
         model.addAttribute("appropriateFacials", appropriateFacials);
         model.addAttribute("nailService", manipedi);
+        model.addAttribute("skinType", skintype);
         return "menu";
     }
 }
